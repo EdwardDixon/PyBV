367aaf88cd508a7b2db35624c6d58f06a76b2a99
Edward Dixon <dixon.edward@gmail.com>
2017-10-03 22:35:24 +0100

    Starting repo again!

---
+++ tests/test_bvtensor.py
@@ -0,0 +1,38 @@
+import unittest
+
+from he.bv.basic import BVTensor
+from he.keys import KeyPair
+import numpy as np
+
+
+class addition_test(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(addition_test, self).__init__(*args, **kwargs)
+        self.keys = KeyPair()
+        self.keys.generate()
+
+    def test_add(self):
+        a = BVTensor(self.keys.pk(), np.asarray([40,2]), True, self.keys.sk())
+
+        # integers only, for now!
+        b = a + 2
+        p = self.keys.sk().decrypt(b)
+        print(p)
+        # TODO decrypt, check plaintext result
+
+
+class multiplication_test(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(multiplication_test, self).__init__(*args, **kwargs)
+        self.keys = KeyPair()
+        self.keys.generate()
+
+    def test_mul_by_scalar(self):
+        a = BVTensor(self.keys.pk(), np.asarray([21]), True, self.keys.sk())
+
+        # Integers only, for now!
+        b = a * 2
+        # TODO decrypt, check plaintext result
+
+if __name__ == "__main__":
+    unittest.main()
